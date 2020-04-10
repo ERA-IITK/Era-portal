000f05bd4d9f16bfa8715f4591a081b869bb9dc4
Shashikala07 <vkshashi@iitk.ac.in>
2020-04-10 13:22:04 +0530

    upload files

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { BackendService } from './../backend.service';
+import { Component, OnInit } from '@angular/core';
+
+ 
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  userloggedin:boolean=true;
+  error: boolean=false;
+  errorMessage:String="";
+  
+
+  constructor(private_backendservice: BackendService) { }
+
+  ngOnInit():  {
+    this.getAuthStatus();
+  }
+  login(filter){
+
+  }
+  logout(){
+  getAuthStatus()
+  }
+
+}
