ce78ab75b532823f6c68be9a2725c7dca3ede916
Shashikala <60825428+Shashikala07@users.noreply.github.com>
2020-04-19 00:43:25 +0530

    Upadated

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,82 +1,73 @@
-import { AuthService } from './backend.service';
-import { AngularFireDatabase } from '@angular/fire/database/database';
 import { User } from './shared/services/user';
-
-import { Injectable , NgModule, NgZone} from '@angular/core';
-import{auth} from 'firebase/app'
-import{AngularFirestore,AngularFirestoreDocument} from '@angular/fire/firestore'
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 
-
-
-import{ Router} from "@angular/router";
-import * as firebase from 'firebase';
+import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
-
-@Injectable({
-   providedIn: 'root'
-})
- 
+@Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  userData: any; //save logged in user data
+    user$: Observable<User>;
+    
+    
 
-  constructor(
-    public afs: AngularFirestore,   // Inject Firestore service
-    public afAuth: AngularFireAuth, // Inject Firebase auth service
-    public router: Router,  
-    public ngZone: NgZone // NgZone service to remove outside scope warning
-  ) {    
-    /* Saving user data in localstorage when 
-    logged in and setting up null when logged out */
-    this.afAuth.authState.subscribe(user => {
-      if (user) {
-        this.userData = user;
-        localStorage.setItem('user', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('user'));
-      } else {
-        localStorage.setItem('user', null);
-        JSON.parse(localStorage.getItem('user'));
-      }
-    })
-  }
-  // Sign in with Google
-  get GoogleAuth() {
-    return this.AuthLogin(new auth.GoogleAuthProvider());
-  }
-
-  // Auth logic to run auth providers
-     AuthLogin(provider) {
-    return this.afAuth.AuthService.signInWithPopup(provider)
-    .then((result) => {
-       this.ngZone.run(() => {
-          this.router.navigate(['landing']);
+    constructor(
+        public afAuth: AngularFireAuth,
+        private afs: AngularFirestore,
+        private router: Router
+    ) {
+      // Get the auth state, then fetch the Firestore user document or return null
+      this.user$ = this.afAuth.authState.pipe(
+        switchMap(user => {
+            // Logged in
+          if (user) {
+            return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+          } else {
+            // Logged out
+            return of(null);
+          }
         })
-      this.SetUserData(result.user);
-    }).catch((error) => {
-      window.alert(error)
-    })
-  }
+      )
 
-  /* Setting up user data when sign in with username/password, 
-  sign up with username/password and sign in with social auth  
-  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
-  SetUserData(user) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
-    const userData: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      emailVerified: user.emailVerified
+
+
+
     }
-    return userRef.set(userData, {
-      merge: true
-    })
-  }
-}
+    async googleSignin() {
+      const provider = new auth.GoogleAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider);
+      return this.updateUserData(credential.user);
+    }
+  
+    private updateUserData(user) {
+      // Sets user data to firestore on login
+      const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+  
+      const data = { 
+        uid: user.uid, 
+        email: user.email, 
+        displayName: user.displayName, 
+        photoURL: user.photoURL
+      } 
+  
+      return userRef.set(data, { merge: true })
+  
+    }
+  
+    async signOut() {
+      await this.afAuth.auth.signOut();
+      this.router.navigate(['/']);
+    }
+     }
+
+
+
 
 
    
