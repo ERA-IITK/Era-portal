9618dff8bee21283f654e4581fcbf697376c81fd
Shashikala <60825428+Shashikala07@users.noreply.github.com>
2020-04-19 00:44:44 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,18 +15,31 @@ import{AngularFireAuthModule} from '@angular/fire/auth';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 
+const config={
+  apiKey: "AIzaSyDkukP6UjX5I_gcP2C0tSEQPiyimRwg6No",
+authDomain: "recruitment-portal-732b0.firebaseapp.com",
+databaseURL: "https://recruitment-portal-732b0.firebaseio.com",
+projectId: "recruitment-portal-732b0",
+storageBucket: "recruitment-portal-732b0.appspot.com",
+messagingSenderId: "820044196096",
+appId: "1:820044196096:web:8d4c20172d144a6c186be5",
+measurementId: "G-2Z03HD317P"
+};
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase,'portal'),
+    AngularFireModule.initializeApp(config),
     AngularFireAuthModule,
     ,
     AngularFirestoreModule,
